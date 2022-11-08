2f3c0dbadf5fbf911727318c018290bc23be817f
liqixin <liqixin@bupt.cn>
2022-11-08 15:01:36 +0800

    修复钟表程序。

---
+++ clock-tinker/src/main/java/com/alibaba/jvm/sandbox/demo/BrokenClockTinkerModule.java
@@ -0,0 +1,51 @@
+package com.alibaba.jvm.sandbox.demo;
+
+/**
+ * @author : liqixin
+ * @create : 2022-09-12 14-56
+ * @Description: 修复钟
+ */
+import com.alibaba.jvm.sandbox.api.Information;
+import com.alibaba.jvm.sandbox.api.Module;
+import com.alibaba.jvm.sandbox.api.ProcessController;
+import com.alibaba.jvm.sandbox.api.annotation.Command;
+import com.alibaba.jvm.sandbox.api.listener.ext.Advice;
+import com.alibaba.jvm.sandbox.api.listener.ext.AdviceListener;
+import com.alibaba.jvm.sandbox.api.listener.ext.EventWatchBuilder;
+import com.alibaba.jvm.sandbox.api.resource.ModuleEventWatcher;
+import org.kohsuke.MetaInfServices;
+
+import javax.annotation.Resource;
+
+@MetaInfServices(Module.class)
+@Information(id = "broken-clock-tinker")
+public class BrokenClockTinkerModule implements Module {
+
+    @Resource
+    private ModuleEventWatcher moduleEventWatcher;
+
+    @Command("repairCheckState")
+    public void repairCheckState() {
+
+        new EventWatchBuilder(moduleEventWatcher)
+                .onClass("com.taobao.demo.Clock")
+                .onBehavior("checkState")
+                .onWatch(new AdviceListener() {
+
+                    /**
+                     * 拦截{@code com.taobao.demo.Clock#checkState()}方法，当这个方法抛出异常时将会被
+                     * AdviceListener#afterThrowing()所拦截
+                     */
+                    @Override
+                    protected void afterThrowing(Advice advice) throws Throwable {
+
+                        // 在此，你可以通过ProcessController来改变原有方法的执行流程
+                        // 这里的代码意义是：改变原方法抛出异常的行为，变更为立即返回；void返回值用null表示
+                        // --liqixin 20221108 这里实际上抛出了异常，但是sandbox利用asm进行了处理 提取了里面的response作为返回值，null值立刻返回
+                        ProcessController.returnImmediately(null);
+                    }
+                });
+
+    }
+
+}
